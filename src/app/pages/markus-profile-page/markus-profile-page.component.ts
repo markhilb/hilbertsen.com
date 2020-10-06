3f70275a9bfd93e640d4879a4f9ba38d4c44c7e1
markhilb <mhilbertsen@hotmail.no>
2020-10-06 19:37:01 +0200

    Implement profile page

--- src/app/pages/markus-profile-page/markus-profile-page.component.ts
+++ src/app/pages/markus-profile-page/markus-profile-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-markus-profile-page',
   templateUrl: './markus-profile-page.component.html',
-  styleUrls: ['./markus-profile-page.component.scss']
+  styleUrls: ['./markus-profile-page.component.scss'],
 })
 export class MarkusProfilePageComponent implements OnInit {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
