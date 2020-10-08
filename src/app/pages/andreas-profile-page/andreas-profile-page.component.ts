12946c89e50b325d4f6fa909725e10c6eb27245f
markhilb <mhilbertsen@hotmail.no>
2020-10-08 22:44:44 +0200

    Add general structure of andreas profile page

--- src/app/pages/andreas-profile-page/andreas-profile-page.component.ts
+++ src/app/pages/andreas-profile-page/andreas-profile-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-andreas-profile-page',
   templateUrl: './andreas-profile-page.component.html',
-  styleUrls: ['./andreas-profile-page.component.scss']
+  styleUrls: ['./andreas-profile-page.component.scss'],
 })
 export class AndreasProfilePageComponent implements OnInit {
+  faEnvelope = faEnvelope;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
