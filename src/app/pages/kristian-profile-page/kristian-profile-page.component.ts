89d68870bf3421d755c1aa78c8ced84ecb46f54e
markhilb <mhilbertsen@hotmail.no>
2020-10-10 13:05:15 +0200

    Implement kristian profile page

--- src/app/pages/kristian-profile-page/kristian-profile-page.component.ts
+++ src/app/pages/kristian-profile-page/kristian-profile-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-kristian-profile-page',
   templateUrl: './kristian-profile-page.component.html',
-  styleUrls: ['./kristian-profile-page.component.scss']
+  styleUrls: ['./kristian-profile-page.component.scss'],
 })
 export class KristianProfilePageComponent implements OnInit {
+  faEnvelope = faEnvelope;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
