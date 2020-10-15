beec80a803e42caf6979fb72027a65a21c7a3a2d
markhilb <mhilbertsen@hotmail.no>
2020-10-15 13:18:46 +0200

    Fix + cleanup page animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -21,7 +21,9 @@ const moveIn = [
     { optional: true },
   ),
 
-  query(':enter', [style({ opacity: 0, transform: 'scale(.7,.7)' })]),
+  query(':enter', [style({ opacity: 0, transform: 'scale(.7,.7)' })], {
+    optional: true,
+  }),
   group([
     query(
       ':leave',
@@ -37,49 +39,14 @@ const moveIn = [
         animate('200ms ease-out', style({ opacity: 1 })),
         animate('500ms ease-out', style({ transform: 'scale(1,1)' })),
       ]),
-    ),
-  ]),
-];
-
-const moveOut = [
-  style({ position: 'relative' }),
-  query(
-    ':enter, :leave',
-    [
-      style({
-        position: 'absolute',
-        height: '100vh',
-        width: '100%',
-      }),
-    ],
-    { optional: true },
-  ),
-
-  query(':enter', [style({ opacity: 0, transform: 'scale(1.1,1.1)' })]),
-  group([
-    query(
-      ':leave',
-      group([
-        animate('200ms ease-out', style({ opacity: 0 })),
-        animate('500ms ease-out', style({ transform: 'scale(.7,.7)' })),
-      ]),
       { optional: true },
-    ),
-    query(
-      ':enter',
-      group([
-        animate('200ms 150ms ease-out', style({ opacity: 1 })),
-        animate('500ms 150ms ease-out', style({ transform: 'scale(1,1)' })),
-      ]),
     ),
   ]),
 ];
 
 const slideLeft = [
   style({ position: 'relative' }),
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
-    optional: true,
-  }),
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' })),
   query(':enter', [style({ right: '-100%' })]),
   group([
     query(':leave', [animate('200ms ease-out', style({ right: '100%' }))]),
@@ -89,9 +56,7 @@ const slideLeft = [
 
 const slideRight = [
   style({ position: 'relative' }),
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
-    optional: true,
-  }),
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' })),
   query(':enter', [style({ left: '-100%' })]),
   group([
     query(':leave', [animate('200ms ease-out', style({ left: '100%' }))]),
